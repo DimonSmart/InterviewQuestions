54dbe2404c37452b97aa9517542c2ca48f833f06
Dmitry Dorogoy <dorogoj@live.ru>
2023-03-29 12:38:56 +0700

    TapeEquilibrium added

---
+++ InterviewQuestions/TapeEquilibrium.cs
@@ -0,0 +1,24 @@
+﻿namespace InterviewQuestions;
+
+public static class TapeEquilibrium
+{
+    public static int Solution(int[] A)
+    {
+        var total = A.Sum(i => (long)i);
+        long left = 0;
+        var right = total;
+        var diff = int.MaxValue;
+        for (int i = 0; i < A.Length - 1; i++)
+        {
+            Console.Write($"p:{i} {left},{right}{Environment.NewLine}");
+            left += A[i];
+            right -= A[i];
+            int newDiff = (int)Math.Abs(left - right);
+            if (newDiff < diff)
+            {
+                diff = newDiff;
+            }
+        }
+        return (int)diff;
+    }
+}
---
+++ InterviewQuestionsTests/TapeEquilibriumTests.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewQuestionsTests;
+
+public class TapeEquilibriumTests
+{
+    [Theory]
+    [InlineData(new int[] { 3, 1, 2, 4, 1 }, 1)]
+    [InlineData(new int[] { -10, 10 }, 20)]
+    public void TapeEquilibriumTest(int[] a, int expected)
+    {
+        Assert.Equal(expected, TapeEquilibrium.Solution(a));
+    }
+}
