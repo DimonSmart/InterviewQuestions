46bf43b181b91b6b549c55308cf2e07bb7abab6c
Dmitry Dorogoy <dorogoj@live.ru>
2023-03-29 09:53:55 +0700

    PermMissingElem added

---
+++ InterviewQuestions/PermMissingElem.cs
@@ -0,0 +1,14 @@
+﻿namespace InterviewQuestions;
+
+public static class PermMissingElem
+{
+    public static int Solution(int[] A)
+    {
+        long l = A.Length;
+        if (l == 0) return 1;
+
+        long sum = A.Sum(i => (long)i);
+        long x = (l + 1L) * (l + 1 + 1) / 2;
+        return (int)(x - sum);
+    }
+}
---
+++ InterviewQuestionsTests/PermMissingElemTests.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewQuestionsTests
+{
+    public class PermMissingElemTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 3, 1, 5 }, 4)]
+        public void FrogJumpTest(int []a, int expected)
+        {
+            Assert.Equal(expected, PermMissingElem.Solution(a));
+        }
+    }
+}
