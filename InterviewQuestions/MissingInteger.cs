291ac01a80cd485a7fe194e5d8af90fb8ae9949b
Dmitry Dorogoy <dorogoj@live.ru>
2023-03-30 13:34:02 +0700

    MissingInteger added

---
+++ InterviewQuestions/MissingInteger.cs
@@ -0,0 +1,23 @@
+﻿namespace InterviewQuestions;
+
+public static class MissingInteger
+{
+    public static int Solution(int[] A)
+    {
+        var a = A.Where(i => i > 0).OrderBy(i => i);
+        var missingNumber = 1;
+        foreach (var i in a)
+        {
+            if (i == missingNumber)
+            {
+                missingNumber++;
+                continue;
+            }
+            if (i > missingNumber)
+            {
+                return missingNumber;
+            }
+        }
+        return missingNumber;
+    }
+}
---
+++ InterviewQuestionsTests/MissingIntegerTests.cs
@@ -0,0 +1,13 @@
+﻿namespace InterviewQuestionsTests;
+
+public class MissingIntegerTests
+{
+    [Theory]
+    [InlineData(new int[] { 1, 3, 6, 4, 1, 2 }, 5)]
+    [InlineData(new int[] { 1, 2, 3 }, 4)]
+    [InlineData(new int[] { -1, -2 }, 1)]
+    public void MissingIntegerTest(int[] a, int expected)
+    {
+        Assert.Equal(expected, MissingInteger.Solution(a));
+    }
+}
