3fa298b599866117b3a8654b7bd9029fb07ac258
Dmitry Dorogoy <dorogoj@live.ru>
2023-03-31 14:15:05 +0700

    PassingCars added

---
+++ InterviewQuestions/PassingCars.cs
@@ -0,0 +1,27 @@
+﻿namespace InterviewQuestions;
+
+public static class PassingCars
+{
+    public static int Solution(int[] A)
+    {
+        var left = 0;
+        var pass = 0;
+
+        foreach (var a in A)
+        {
+            if (a == 0)
+            {
+                left++;
+                continue;
+            }
+            pass += left;
+            if (pass > 1_000_000_000)
+            {
+                return -1;
+            }
+
+        }
+
+        return pass;
+    }
+}
---
+++ InterviewQuestionsTests/PassingCarsTests.cs
@@ -0,0 +1,11 @@
+﻿namespace InterviewQuestionsTests;
+
+public class PassingCarsTests
+{
+    [Theory]
+    [InlineData(new int[] { 0, 1, 0, 1, 1 }, 5)]
+    public void PassingCarTest(int[] a, int expected)
+    {
+        Assert.Equal(expected, PassingCars.Solution(a));
+    }
+}
