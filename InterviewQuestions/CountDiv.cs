59959d0f68430ae74286051ae039a6ebb1501b99
Dmitry Dorogoy <dorogoj@live.ru>
2023-04-03 22:59:43 +0700

    CountDiv added

---
+++ InterviewQuestions/CountDiv.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewQuestions;
+
+/// <summary>
+/// https://app.codility.com/programmers/lessons/5-prefix_sums/count_div/
+/// </summary>
+public static class CountDiv
+{
+    public static int Solution(int A, int B, int K)
+    {
+        return B / K - A / K + (A % K == 0 ? 1 : 0);
+    }
+}
---
+++ InterviewQuestionsTests/CountDivTests.cs
@@ -0,0 +1,14 @@
+﻿namespace InterviewQuestionsTests
+{
+    public class CountDivTests
+    {
+        [Theory]
+        [InlineData(6, 11, 2, 3)]
+        [InlineData(11, 345, 17, 20)]
+        public void CountDivTest(int a, int b, int k, int expected)
+        {
+            Assert.Equal(expected, CountDiv.Solution(a, b, k));
+        }
+    }
+    
+}
