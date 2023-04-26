2a15c66d11f80d68da1307d9b8d25ceafe06b40c
Dmitry Dorogoy <dorogoj@live.ru>
2023-04-26 13:29:38 +0700

    Distinct task added

---
+++ InterviewQuestions/Distinct.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewQuestions;
+
+/// <summary>
+/// https://app.codility.com/programmers/lessons/6-sorting/distinct/
+/// </summary>
+public static class Distinct
+{
+      public static int Solution(int[] A)
+      {
+          return A.Distinct().Count();
+      }
+}
---
+++ InterviewQuestionsTests/DistinctTests.cs
@@ -0,0 +1,12 @@
+﻿namespace InterviewQuestionsTests
+{
+    public class DistinctTests
+    {
+        [Theory]
+        [InlineData(new int[] { 2, 1, 1, 2, 3, 1 }, 3)]
+        public void DistinctTest(int[] a, int expected)
+        {
+            Assert.Equal(expected, Distinct.Solution(a));
+        }
+    }
+}
